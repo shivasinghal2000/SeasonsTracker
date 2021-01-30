3bfa72aecc90d0b8832c7ef5246a0740ad47783f
shivasinghal2000 <singhalshiva936@gmail.com>
2021-01-30 20:40:13 +0530

    alternative method to declare state

--- src/App.js
+++ src/App.js
@@ -5,19 +5,43 @@ import ReactDOM from 'react-dom'
 class App extends Component {
   constructor (props) {
     super(props)
-    this.state = { latitude: null }
+    // this.state = { latitude: null, errorMessage: '' }
+  }
+  state = { latitude: null, errorMessage: '' } 
+  componentDidMount () {
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         this.setState({ latitude: position.coords.latitude })
       },
       (err) => {
-        return console.log(err)
+        // return console.log(err)
+        this.setState({ errorMessage: err.message })
       }
     )
   }
 
   render () {
-    return <div>Latitude : {this.state.latitude}</div>
+    if (this.state.errorMessage && !this.state.latitude) {
+      return (
+        <div>
+          Error :  {this.state.errorMessage}
+        </div>
+      )
+    }
+    if (!this.state.errorMessage && this.state.latitude) {
+      return (
+        <div>
+          Latitude : {this.state.latitude}
+        </div>
+      )
+    }
+    if (!this.state.errorMessage && !this.state.latitude) {
+      return (
+        <div>
+          Loading...
+        </div>
+      )
+    }
   }
 }
 
